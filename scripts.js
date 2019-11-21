44786e0435e275dca9e372dbe73ebe1d96f14652
bartoszulc01 <48764271+bartoszulc01@users.noreply.github.com>
2019-11-21 16:36:16 +0100

    push

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
 
+
+    $(window).scroll(function(){
+        var sc=$(window).scrollTop();
+        if(sc>100){
+            $(".navbar").addClass("sticky");
+        }
+        else{
+            $(".navbar").removeClass("sticky");
+        }
+
+
+    });
+});
